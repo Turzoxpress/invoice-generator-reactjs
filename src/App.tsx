b8d5f906e695263c77d651b6f73b009002139a13
Mahbubur Rahman Turzo <turzo@newgen-bd.com>
2022-07-19 13:09:50 +0600

    initial release

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InvoicePage from "./components/InvoicePage";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="center fs-30">Invoice Generator</h1>
+      <InvoicePage />
+    </div>
+  );
+}
+
+export default App;
